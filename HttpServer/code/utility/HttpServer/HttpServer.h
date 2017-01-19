eefeab864fa0713fd4bafa0119b456e8cee3542f
yapingxin <xinyp@live.com>
2017-01-19 09:49:18 +0800

    HttpServer/code/utility/HttpServer/HttpServer.h

---
+++ HttpServer/code/utility/HttpServer/HttpServer.h
@@ -0,0 +1,6 @@
+#ifndef CODE_UTILITY_HttpServer_HttpServer_H 
+#define CODE_UTILITY_HttpServer_HttpServer_H 
+
+
+
+#endif // CODE_UTILITY_HttpServer_HttpServer_H
--- HttpServer/main.c
+++ HttpServer/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+#include "code/utility/HttpServer/HttpServer.h"
+
 int main()
 {
-    printf("hello from HttpServer!\n");
-    return 0;
+	printf("hello from HttpServer!\n");
+	return 0;
 }
+
+
